3e955c0b965e6e3b43211213c3dd437fe47f8806
juan felipe londoño arbelaez <piplondona@gmail.com>
2022-06-13 22:01:35 -0500

    Commit

---
+++ MercadoLibre/MercadoLibre.Mutant/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine(String.Format("Please, type your dna, en el siguiente formato: {0}", "valor1, valor2"));
+string? dna = Console.ReadLine();
+try
+{
+    //if (dna)
+    string[] dnaArray = dna.Split(',');
+    isMutant(dnaArray);
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(String.Format("Error: description: {0}", ex.ToString()));       
+    throw;
+}
+
+static bool isMutant(string[] dna)
+{
+    return true;
+}
